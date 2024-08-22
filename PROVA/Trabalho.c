f19d02214fa550d6814823228b4f1f5484c1c63e
brunosouza000 <bruno.souza20397@gmail.com>
2024-08-21 21:34:25 -0300

    prova

--- PROVA/Trabalho.c
+++ PROVA/Trabalho.c
@@ -8,46 +8,50 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include <stdio.h>
 
-        double soma(double A, double B);
-        
+double soma(double A, double B);
+
 int main()
 {
-        
-        char mensagem [100];
-        
+    double X, Y;
+    char mensagem [100];
+
     printf("selecione uma das operacoes:\n");
     printf("1 -soma\n");
     printf("2 -somasubtracao\n");
     printf("3 -multiplicacao\n");
     printf("4 -divisao\n");
-    printf("5 -potenciacao\n");    
+    printf("5 -potenciacao\n");
     printf("\n");
-    
-    
-    
+
+
+
 fgets(mensagem, sizeof(mensagem), stdin);
     printf("\n");
-    
+
     printf("voce selecionou: %s\n", mensagem);
-    
+
     printf("insira os valores:\n");
-    
-    printf("\n");
-    
+
+   printf("\n");
+
     //-soma
-    
+
     //scanf("%lf\n%lf", &A, &B);
-    
+
     //soma = A+B;
-    
+
     printf("\n");
-    printf("resultado = %lf\n", soma(1,2));
-    
+    scanf("%lf\n%lf",&X,&Y);
+    printf("resultado = %lf\n", soma(X,Y));
+
     return 0;
 }
 
 double soma(double A, double B)
 {
-    scanf("%lf\n%lf", &A, &B);
-    
+    double resultado;
+
+    resultado = A+B;
+    return(resultado);
+
 }
